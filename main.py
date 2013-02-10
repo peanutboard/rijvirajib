1b828c02b9a95b35aacd9cd646ac18dc7f0403b7
Kaila Hutchins <kaila.hutchins@gmail.com>
2013-02-10 18:55:08 -0500

    Update for personal site
    Updated for Personal Site along with e-mail

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
+from google.appengine.api import mail
 
 class Main(webapp.RequestHandler):
     def get(self):
@@ -25,9 +26,24 @@ class Main(webapp.RequestHandler):
     def write(self, d):
         self.response.out.write(d)
 
+class Email(webapp.RequestHandler):
+    def post(self):
+        mail.send_mail(sender='kaila.hutchins@gmail.com',
+            to='Kaila Hutchins <kaila.hutchins@gmail.com>',
+            subject=self.request.get('subject'),
+            body='From: ' + self.request.get('name') + '\nSubject: ' + self.request.get('subject') + '\nMessage: ' + self.request.get('message'))
+        
+        '''
+        self.response.out.write('<html><body>You wrote:<pre>')
+        self.response.out.write(self.request.get('message'))
+        self.response.out.write('</pre></body></html>')
+        '''
+        self.response.out.write('OK')
+        
 
 application = webapp.WSGIApplication([
     ('/', Main),
+    ('/contact', Email),
 ], debug=True)
 
 def main():
